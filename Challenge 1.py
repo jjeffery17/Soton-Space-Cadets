9f348cb039e9e5784b28bc5b47037feb1696f232
James <jjeffery17@heathfieldcc.co.uk>
2024-10-07 16:04:41 +0100

    added comments

--- Challenge 1.py
+++ Challenge 1.py
@@ -2,13 +2,13 @@ import requests as req
 
 id = input("Enter email ID: ")
 if "@" in id:
-    id = id.split("@")[0]
+    id = id.split("@")[0]  # format input
 
 try:
-    res = req.get("https://www.ecs.soton.ac.uk/people/"+id)
+    res = req.get("https://www.ecs.soton.ac.uk/people/"+id)  # get short redirect address
     address = res.history[0].headers["location"]
-    res = req.get(address)
-    print(res.url.split("/")[-1].replace("-", " ").title())
+    res = req.get(address)  # get full redirect address
+    print(res.url.split("/")[-1].replace("-", " ").title())  # format and output name
 except Exception as e:
     print("Error, could not get name.")
     print("Error Info:", e)
